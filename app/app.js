698f23eadf3589c8b09d6a3f3b33aad971255e08
Musema Hassen <musema.hassen@gmail.com>
2022-01-30 18:34:42 -0800

    Add modal

--- app/app.js
+++ app/app.js
@@ -1,10 +1,27 @@
 
-const profileImage = document.getElementById('profile-picture');
+
 const images = [
     "./images/musema-1.jpeg",
     "./images/musema-2.png",
     "./images/musema-3.png"
-]
+];
+
+const profileImage = document.getElementById('profile-picture');
+const modal = document.getElementById('modal');
+const modalCloseButton = document.getElementById('modal-close-button');
+const modalContentImage = document.getElementById('modal-content-image');
+
+const showPreviousImageButton = document.getElementById('show-previous-image-button');
+const showNextImageButton = document.getElementById('show-next-image-button');
+
+
+
+modalCloseButton.addEventListener('click', closeModal);
+profileImage.addEventListener('click', showModal);
+showPreviousImageButton.addEventListener('click', prevModalImage);
+showNextImageButton.addEventListener('click', nextModalImage);
+window.addEventListener('click', onWindowClick);
+
 if(profileImage) {
     let imageIndex = 0;
     setInterval(() => {
@@ -17,7 +34,55 @@ if(profileImage) {
         }
     }, 5000);
 
-    profileImage.addEventListener('click', function(){
+    profileImage.addEventListener('click', handleProfileImageClick)
+}
 
-    })
+
+
+
+// modal actions
+let currentModalImageIndex;
+function handleProfileImageClick(e) {
+    const imageSrc = e.target.src;
+    modalContentImage.src = imageSrc;
+    currentModalImageIndex = getImageIndex(imageSrc);
+}
+
+function closeModal(){
+    modal.style.display = 'none'
+}
+
+function showModal(){
+    modal.style.display = 'block'
+}
+
+function nextModalImage(){
+    if(currentModalImageIndex >= images.length-1){
+        currentModalImageIndex = 0;
+    } else{
+        currentModalImageIndex++;
+    }
+    setModalImageSrc();
+}
+
+function prevModalImage(){
+    if(currentModalImageIndex <= 0){
+        currentModalImageIndex = images.length -1;
+    } else{
+        currentModalImageIndex--;
+    }
+    setModalImageSrc();
+}
+function getImageIndex(src) {
+    return images.findIndex(imageSrc => src.indexOf(imageSrc) >= 0);
+}
+
+function setModalImageSrc() {
+    modalContentImage.src = images[currentModalImageIndex]
+}
+
+function onWindowClick(e) {
+    if(e.target == modal){
+        closeModal();
+    }
 }
